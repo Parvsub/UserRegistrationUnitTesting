3726ff510011a69d18c35e94ff5762c0d64025a9
Parvsub <Praveenboda421@gmail.com>
2023-03-05 18:02:56 +0530

    Added:Last Name Test is conducted Updated

--- userregistrationTest/UserRegistrationTest.java
+++ userregistrationTest/UserRegistrationTest.java
@@ -1,31 +1,32 @@
 package com.userregistrationTest;
 
 import org.junit.Assert;
-import org.junit.TestL;
+import org.junit.Test;
 
 import java.util.regex.Pattern;
 
 public class UserRegistrationTest {
     @Test
-    public void givenLastName_WhenProper_ShouldReturn_True() {
+    public void givenFirstName_WhenProper_ShouldReturn_True() {
         UserValidater userValidater = new UserValidater();
-        boolean result = userValidater.validateLastName("Kumar");
+        boolean result = userValidater.validateFirstName("Kumar");
         Assert.assertEquals(true,result);
     }
     @Test
-    public void givenLastName_WhenNotProper_ShouldReturn_False() {
+    public void givenFirstName_WhenNotProper_ShouldReturn_False() {
         UserValidater userValidater = new UserValidater();
-        boolean result = userValidater.validateLastName("ku");
+        boolean result = userValidater.validateFirstName("K");
         Assert.assertEquals(false,result);
     }
 }
 
 
- class UserValidater {
-     private static final String Last_name = ("[A-Z]{1}[a-z]+");
+class UserValidater {
+    private static final String FIRST_NAME_PATTERN = "[A-Z]{1}[a-z]+";
 
-     public boolean validateLastName(String last_name) {
-         Pattern pattern = Pattern.compile(Last_name);
-         return pattern.matcher(Last_name).matches();
-     }
- }
+    public boolean validateFirstName(String firstName) {
+        Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
+        return pattern.matcher(firstName).matches();
+    }
+}
+
