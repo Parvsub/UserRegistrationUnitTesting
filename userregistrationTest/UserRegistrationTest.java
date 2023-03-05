63451beb811061dba0bb8107be91ffe2022ef3c3
Parvsub <Praveenboda421@gmail.com>
2023-03-05 18:13:09 +0530

    Added:Email Test is conducted Updated

--- userregistrationTest/UserRegistrationTest.java
+++ userregistrationTest/UserRegistrationTest.java
@@ -9,24 +9,24 @@ public class UserRegistrationTest {
     @Test
     public void givenFirstName_WhenProper_ShouldReturn_True() {
         UserValidater userValidater = new UserValidater();
-        boolean result = userValidater.validateFirstName("Kumar");
+        boolean result = userValidater.validateEmail("abc.xyz@bl.co.in");
         Assert.assertEquals(true,result);
     }
     @Test
     public void givenFirstName_WhenNotProper_ShouldReturn_False() {
         UserValidater userValidater = new UserValidater();
-        boolean result = userValidater.validateFirstName("K");
+        boolean result = userValidater.validateEmail("Praveen@.com");
         Assert.assertEquals(false,result);
     }
 }
 
 
 class UserValidater {
-    private static final String FIRST_NAME_PATTERN = "[A-Z]{1}[a-z]+";
+    private static final String Email= "[abc]{3}.[a-z]{3}+@[bl]{2}.[co]{2}.[a-z]{2}+";
 
-    public boolean validateFirstName(String firstName) {
-        Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
-        return pattern.matcher(firstName).matches();
+    public boolean validateEmail(String Email) {
+        Pattern pattern = Pattern.compile(Email);
+        return pattern.matcher(Email).matches();
     }
 }
 
