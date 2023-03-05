ce723cff5c56e85ad3d5a377a270eafcc44f9e34
Parvsub <Praveenboda421@gmail.com>
2023-03-05 15:55:10 +0530

    Added:First Name Test is conducted

---
+++ userregistrationTest/UserRegistrationTest.java
@@ -0,0 +1,31 @@
+package com.userregistrationTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.regex.Pattern;
+
+public class UserRegistrationTest {
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturn_True() {
+        UserValidater userValidater = new UserValidater();
+        boolean result = userValidater.validateFirstName("Praveen");
+        Assert.assertEquals(true,result);
+    }
+    @Test
+    public void givenFirstName_WhenNotProper_ShouldReturn_False() {
+        UserValidater userValidater = new UserValidater();
+        boolean result = userValidater.validateFirstName("P");
+        Assert.assertEquals(false,result);
+    }
+}
+
+
+ class UserValidater {
+     private static final String FIRST_NAME_PATTERN = "[A-Z]{1}[a-z]+";
+
+     public boolean validateFirstName(String firstName) {
+         Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
+         return pattern.matcher(firstName).matches();
+     }
+ }
